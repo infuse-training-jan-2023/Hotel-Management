7e646ae40e0491061701f58f9f92e9e725837f7a
2124rohan <vrohan1360@gmail.com>
2023-02-13 21:30:11 +0530

    added unit test for invoice

--- server/unit_test/test_invoice.py
+++ server/unit_test/test_invoice.py
@@ -6,3 +6,8 @@ def test_invoice_review(mocker):
     mock = mocker.patch('service.invoice_service.Invoice.get_invoice', return_value = data)
     review_returned = Invoice.get_invoice()
     assert data == review_returned  
+
+def test_invoice_review_makes_db_call(mocker):
+  mock = mocker.patch('service.invoice_service.Invoice.get_invoice', return_value = [])
+  _ = Invoice.get_invoice()
+  assert mock.call_count == 1
