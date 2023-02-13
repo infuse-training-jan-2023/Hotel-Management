9efe5cb54563688d7431c9518a8f3025c6f0d859
Leon <mca.2016@unigoa.com>
2023-02-13 12:51:50 +0530

    deleted db and model file

--- server/DB/connect.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-class Connection:
-    cluster = MongoClient('mongodb+srv://mongodb:mongodb@cluster0.uxi24a7.mongodb.net/?retryWrites=true&w=majority')
-    db = cluster['hotel']
-    customer = db.customer
-    room = db.room
-    booking = db.booking
