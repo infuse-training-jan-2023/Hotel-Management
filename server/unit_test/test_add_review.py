c1c1390fec0e107877e901fcb33769fb5e6993de
2124rohan <vrohan1360@gmail.com>
2023-02-13 21:23:00 +0530

    added unit tests for add review, get_all_review and invoice

--- server/unit_test/test_add_review.py
+++ server/unit_test/test_add_review.py
@@ -1,18 +1,14 @@
-import sys
-sys.path.insert(0, './service')
-from bson.objectid import ObjectId
-from customer_review_service import Review
+from service.customer_review_service import Review
 
-data = {"rating":2,"feedback":"recommended","name":"rohan","_id":ObjectId('63e68ea543eefbbf88459d29')}
+data = (2,"recommended","rohan",'63e68ea543eefbbf88459d29')
 
 def test_add_review_makes_db_call(mocker):
-
-  mock = mocker.patch('customer_review_service.Review.customer_review', return_value = [])
+  mock = mocker.patch('service.customer_review_service.Review.customer_review', return_value = [])
   _ = Review.customer_review(data)
   assert mock.call_count == 1
 
 
 def test_customer_review(mocker):
-    mock = mocker.patch('services.customer_review_service.Review.customer_review', return_value = result)
-    msg_returned = Review.customer_review()
-    assert result == msg_returned  
+    mock = mocker.patch('service.customer_review_service.Review.customer_review', return_value = data)
+    review_returned = Review.customer_review()
+    assert data == review_returned  
---
+++ server/unit_test/test_invoice.py
@@ -0,0 +1,8 @@
+from service.invoice_service import Invoice
+
+data = '63ea0a2a75a84883f4662ee0'
+
+def test_invoice_review(mocker):
+    mock = mocker.patch('service.invoice_service.Invoice.get_invoice', return_value = data)
+    review_returned = Invoice.get_invoice()
+    assert data == review_returned  
