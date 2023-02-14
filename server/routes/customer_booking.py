cc693b7c6d01b5174f377336003e071372c398a5
2124rohan <vrohan1360@gmail.com>
2023-02-14 10:32:48 +0530

    refactor code

--- server/routes/customer_booking.py
+++ server/routes/customer_booking.py
@@ -3,6 +3,7 @@ import json
 import sys
 sys.path.insert(0, './controller')
 from booking import BookingController
+from bson import json_util
 
 booking_bp=Blueprint('booking_bp',__name__)
 @booking_bp.route("/api/loyalty-discount", methods = ['GET'])
@@ -11,3 +12,10 @@ def calc_discount():
     id = request.args.get('id')
     discount = booking.calculate_discount(id)
     return Response(json.dumps({"discount":discount}), status=200, mimetype="application/json")
+
+@booking_bp.route("/api/customer_booking", methods = ['GET'])
+def get_all_bookings():
+    booking = BookingController()
+    id = request.args.get('id')
+    all_booking = booking.get_all_bookings(id)
+    return Response(json_util.dumps(all_booking), status=200, mimetype="application/json")
