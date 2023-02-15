dc8447c87f87ead7b2736d231b77eaea32f305da
2124rohan <vrohan1360@gmail.com>
2023-02-15 10:37:44 +0530

    refactoring done

--- server/routes/search.py
+++
@@ -1,9 +0,0 @@
-# from flask import Blueprint, Response
-# from bson import json_util
-# from controller.search_room import SearchController
-
-# search_bp=Blueprint('search_bp',__name__)
-# @search_bp.route("/search", methods = ['POST'])
-# def get_all_rooms():
-#     rooms = SearchController().get_all_rooms()
-#     return Response(json_util.dumps(rooms), status=201, mimetype="application/json")
