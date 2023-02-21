e181e055cc2042a69297d3061f386bb6dccba24c
Tejas Lotlikar <mrt3j45@gmail.com>
2023-02-21 11:58:04 +0530

    added config for server build

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from routes.add_on_routes import add_on_bp
 from routes.bookings_routes import bookings_bp
 from routes.discount_route import discount_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 app.register_blueprint(review_bp,url_prefix='/api')
 app.register_blueprint(pdf_bp,url_prefix='/api')
@@ -16,5 +16,9 @@ app.register_blueprint(bookings_bp,url_prefix='/api')
 app.register_blueprint(discount_bp,url_prefix='/api')
 
 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+    
 if __name__=='__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')
